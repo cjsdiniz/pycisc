50a3cff84688b995467bc79b7fbd8ea14589651c
Carlos Diniz <67067863+cjsdiniz@users.noreply.github.com>
2023-03-23 15:29:53 +0000

    Update nested_data.py

--- intro-python/parsing-json/nested_data.py
+++ intro-python/parsing-json/nested_data.py
@@ -25,6 +25,7 @@ SOFTWARE.
 
 import json
 import os
+from pprint import pprint
 
 
 # Get the absolute path for the directory where this file is located "here"
@@ -33,7 +34,14 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, "interfaces.json")) as file:
     # TODO: Parse the contents of the JSON file into a variable
+        json_text = file.read()
+    
+json_data = json.loads(json_text)
 
 
 # TODO: Loop through the interfaces in the JSON data and print out each
 # interface's name, ip, and netmask.
+# pprint(json_data["ietf-interfaces:interfaces"])
+print("\nInterfaces:\n")
+for interface in json_data["ietf-interfaces:interfaces"]["interface"]:
+    print(interface["name"],interface["ietf-ip:ipv4"]["address"][0]["ip"],interface["ietf-ip:ipv4"]["address"][0]["netmask"])
